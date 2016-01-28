e25cfcccb3437698a8f22de8769b448c9ebd03d7
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2016-01-28 11:17:50 +0100

    Add firmware for the Bamboo 3rd gen (CTH-470)
    The distance is still inverted, but otherwise, works OK.
    Still the pad not supported, as mentioned in the TODO.md.
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>

--- devices.py
+++ devices.py
@@ -239,3 +239,58 @@ intuos_Pro_S_touch = Wacom(
 	},
 	original_reports = "23 06 00 ff 09 01 a1 01 85 02 09 01 15 00 26 ff 00 75 08 95 3f 81 02 c0"
 )
+
+bambo_cth470_S_pen = Wacom(
+	name = "Wacom Bamboo 16FG 4x5 Pen",
+	product_id = 0x00de,
+	reports = (
+		bpt3_boot_mouse_collection,
+		bpt3_pen_msg,
+	),
+	logical_mins = {
+	},
+	logical_maxs = {
+		"X": 14720,
+		"Y": 9200,
+		"Tip Pressure": 1023,
+		"Z": 31,
+	},
+	physical_mins = {
+	},
+	physical_maxs = {
+		"X": 1575,
+		"Y": 984,
+	},
+	original_reports = "92 05 01 09 02 a1 01 85 01 09 01 a1 00 05 09 19 01 29 05 15 00 25 01 95 05 75 01 81 02 95 01 75 03 81 01 05 01 09 30 09 31 15 81 25 7f 75 08 95 02 81 06 c0 c0 05 0d 09 01 a1 01 85 02 a1 00 06 00 ff 09 01 15 00 26 ff 00 75 08 95 09 81 02 c0 09 01 85 02 95 01 b1 02 09 01 85 03 95 01 b1 02 09 01 85 04 95 01 b1 02 09 01 85 05 95 01 b1 02 09 01 85 10 95 02 b1 02 09 01 85 11 95 10 b1 02 09 01 85 13 95 01 b1 02 09 01 85 20 95 01 b1 02 09 01 85 21 95 01 b1 02 09 01 85 14 95 1f b1 02 09 01 85 30 95 1f b1 02 09 01 85 31 95 04 b1 1f 85 c0 09 00 95 09 81 02 c0",
+)
+
+bambo_cth470_S_touch = Wacom(
+	name = "Wacom Bamboo 16FG 4x5 Finger",
+	product_id = 0x00de,
+	reports = (
+		bpt3_touch_msg_header,
+		bpt3_touch_msg_finger,
+		bpt3_touch_msg_finger,
+		bpt3_touch_msg_finger,
+		bpt3_touch_msg_finger,
+		bpt3_touch_msg_finger,
+		bpt3_touch_msg_finger,
+		bpt3_touch_msg_finger,
+		bpt3_touch_msg_footer,
+	),
+	logical_mins = {
+	},
+	logical_maxs = {
+		"X": 4096,
+		"Y": 4096,
+		"Width": 4096,
+		"Height": 4096,
+	},
+	physical_mins = {
+	},
+	physical_maxs = {
+		"X": 1575,
+		"Y": 984,
+	},
+	original_reports = "29 06 00 ff 09 80 a1 02 85 02 75 08 95 3f 15 00 26 00 10 81 01 85 03 75 08 95 3f 81 01 c0",
+)
